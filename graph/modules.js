d805ff460d107476c140c47bbe97bcf4fb621a47
I7OLTEPrejCT <51535113+I7OLTEPrejCT@users.noreply.github.com>
2019-10-21 17:33:13 +0300

    Прога парсит файлы, но пока искать по ним не хочет

--- graph/modules.js
+++ graph/modules.js
@@ -1,21 +1,27 @@
 function ModuleProcessor() {
-	function Module(aModuleName, aFileType, aFile) {
+	function Module(aModuleName, aFileType, aFile, modName) {
 		this.type;
 		this.files = {};
+		this.modName;
 		this.check = function(aModuleName, aFileType, aFile) {
 			this.files[aFileType] = aFile;
 			if (aFileType === 'js') {
 				this.type = 'module';
 				this.files.js = aFile;
+				this.modName = modName;
 				console.log('Found module: ' + aModuleName);
+				// console.log(this.files);
 			}
 			if (aFileType === 'sql') {
 				this.type = 'query';
 				this.files.sql = aFile;
+				this.modName = modName;
 				console.log('Found query: ' + aModuleName);
 			}
 			if (aFileType === 'model') {
+				this.type = 'model';
 				this.files.model = aFile;
+				this.modName = modName;
 				console.log('Found model file: ' + aModuleName);
 			}
 		};
@@ -23,12 +29,12 @@ function ModuleProcessor() {
 		this.check(aModuleName, aFileType, aFile)
 	}
 	this.modules = {};
-	this.process = function(aFilePath, fileName) {
+	this.process = function(aFilePath, fileName, modName) {
 		var raw = fileName.split('.');
 		var fileType = raw[raw.length-1];
 		var moduleName = fileName.slice(0, fileName.length - raw[raw.length-1].length - 1);
 		if (!this.modules[moduleName])
-			this.modules[moduleName] = new Module(moduleName, fileType, aFilePath);
+			this.modules[moduleName] = new Module(moduleName, fileType, aFilePath, modName);
 		else
 			this.modules[moduleName].check(moduleName, fileType, aFilePath);
 	}
--- graph/search.js
+++ graph/search.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const fs = require('fs');
 const async = require('async');
+const splt = require('split');
 const ModuleProcessor = require('./modules');
 
 function getFiles(dirPath, callback) {
@@ -12,6 +13,7 @@ function getFiles(dirPath, callback) {
 		let modules = new ModuleProcessor();
 		async.eachSeries(files, function (fileName, eachCallback) {
 			let filePath = path.join(dirPath, fileName);
+			let modName = 'nothing';
 			//let a = fileName.split('.');
 			//console.log(a[0]);
 
@@ -28,10 +30,22 @@ function getFiles(dirPath, callback) {
 
 				} else {
 					if (stat.isFile() && (/\.js$/.test(filePath)
-						|| /\.sql$/.test(filePath))
-						|| /\.model$/.test(filePath)) {
-						modules.process(filePath, fileName);
-						// filePaths.push(fileName);
+						|| /\.sql$/.test(filePath)
+						|| /\.model$/.test(filePath))) {
+						filePaths.push(fileName);
+						fs.createReadStream(filePath)
+							.pipe(splt())
+							.on('data', function (line) {
+								var nameString = line.match(/[\s\S]*@name\S*\s*/g);
+								if (nameString) {
+									modName = line.slice(nameString[0].length, line.length);
+								}
+								//fs.appendFileSync('./log.txt', fileName+' '+nameString+'\n');
+								//each chunk now is a separate line!
+						});
+						modules.process(filePath, fileName, modName);
+
+
 						//console.log(filePaths);
 					}
 
@@ -45,9 +59,9 @@ function getFiles(dirPath, callback) {
 	});
 
 }
+//E:\arm-master
 
-
-getFiles('./', function (err, files) {
+getFiles('E:\\p3-to-p5-converter\\graph', function (err, files) {
 
 	//console.log(err || files);
 });
